c2a860a110462f02541d60e9f6c3c541a39479e9
Mohamed072005 <amine072005@gmail.com>
2023-09-18 14:11:01 +0100

    C Function

---
+++ Desktop/Cfunction/Cfunction2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Somme();
+int main()
+{
+
+    printf("le somme: %d", Somme(5, 34));
+    return 0;
+}
+int Somme(int a, int b)
+{
+
+    return a + b;
+
+}
